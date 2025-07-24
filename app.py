5c5a647f495c16ba26cb518fb095fc7d3a16e0ae
Ahmet Selim Dostabakan <154963077+adostabakan@users.noreply.github.com>
2025-07-24 10:55:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Render PORT değişkenini algılar
+    app.run(host='0.0.0.0', port=port)         # Gerekli host ve port ayarı
